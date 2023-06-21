f90f84a36cedddb12140fc7a7591ca299669c187
Eric Driussi <ericdriussi@gmail.com>
2023-06-21 08:14:29 +0100

    add more about arrays

--- core/vars.sh
+++ core/vars.sh
@@ -15,12 +15,21 @@ random_var="Hi there!"
 echo "Safely quoted random var:"
 echo "$random_var might work"
 # echo "$random_varor it might not"
-echo "To expand in a string use${random_var}and you'll be safe"
+echo "To expand in a variable use${random_var}and you'll be safe"
 # When in doubt, use ${} to ensure shell expansion
 
 # Declare arrays like so
 my_array=(1 "2" 4 "hey!" 8)
 echo "Array element num 3 is: ${my_array[3]}"
+echo "Each element separately: ${my_array[@]}"
+echo "All elements as a single string: ${my_array[*]}"
+# Add to array
+my_array+=("stuff")
+# Remove from array
+unset "${my_array[-1]}" # On modern Bash, use -1 to get the last element
+# Declare maps like so
+my_map=(["one"]="foo" ["two"]="bar" ["three"]="baz")
+# Same usage as an array, with key instead of index
 
 # Store the output of a command in a variable with $()
 cmd_output=$(whoami)
