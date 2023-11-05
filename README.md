2189885a759286746bdb3fe7df488ff17a5fb192
Eric Driussi <ericdriussi@gmail.com>
2023-11-05 15:14:05 +0000

    use bashunit for tests

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Shell script template
 
-> Simple template with some shell scripting best practices.
+> Simple shell scripting 101 template.
 
-This template is little more than an expansion and example of the information found in [this post](https://sharats.me/posts/shell-script-best-practices) by @sharat87 and in [the bash bible](https://github.com/dylanaraps/pure-bash-bible).
+This template is little more than an expansion and example of the information found
+in [this post](https://sharats.me/posts/shell-script-best-practices) by @sharat87
+and in [the bash bible](https://github.com/dylanaraps/pure-bash-bible).
 
-Simply run `./template.sh` and/or `./template.sh --help` and explore the script, and it's output!
+Simply run `./template.sh` and/or `./template.sh --help` and explore the
+script, and it's output!
 
-To run the tests, you'll need to [install](https://github.com/bats-core/bats-core) `bats-core`, `bats-support` and `bats-assert`.
-You might need to adapt lines 3 and 4 of `test_sample.sh` depending on your distribution.
+To run the tests, you'll need to run
+`curl -s https://bashunit.typeddevs.com/install.sh | bash` to install
+[bashunit](https://bashunit.typeddevs.com/).
+
+Once that's done, just run `./lib/bashunit ./tests/*` and explore `test_sample.sh`!
--- tests/test_sample.sh
+++ tests/test_sample.sh
@@ -1,37 +1,58 @@
 #!/usr/bin/env bash
-setup() {
-  load '/usr/lib/bats-support/load'
-  load '/usr/lib/bats-assert/load'
-    # use $BATS_TEST_FILENAME to grab current file
-    # get the containing directory of this file
-    DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
-    # make executables in template visible to PATH
-    PATH="$DIR/../:$PATH"
-    PATH="$DIR/../core/:$PATH"
+function set_up() {
+	# This will run before each test in this file
+	true
 }
 
-@test "test that it runs" {
-    template.sh
+function set_up_before_script() {
+	# This will run before all tests in this file
+	true
 }
 
-@test "test the output" {
-    run template.sh
-    assert_output --partial "Array element num 3 is: hey!"
+function something_to_test() {
+	echo "himom!"
 }
 
-@test "a test to skip" {
-    skip "Explain why"
-    run template.sh
-    assert_output --partial "Array element num 3 is: hey!"
+function test_should_validate_equal_strings() {
+	result=$(something_to_test)
+	assert_equals "himom!" "$result"
 }
 
-@test "create a tmp file" {
-    touch tmp_file
-    run template.sh
-    assert_output --partial "Array element num 3 is: hey!"
+function test_should_validate_string_contains() {
+	result=$(something_to_test)
+	assert_contains "mom" "$result"
 }
 
-teardown() {
-    # Remove the tmp file
-    rm -f tmp_file
+function test_should_validate_if_dir() {
+	skip "Don't feel like running"
+	local directory="/var"
+	assert_is_directory "$directory"
+}
+
+# more assertions here:
+# https://bashunit.typeddevs.com/assertions
+
+function test_with_mocks() {
+	mock ls echo "what"
+
+	assert_equals "what" "$(ls)"
+}
+
+function test_with_spy() {
+	spy lolcow
+
+	lolcow foo bar
+
+	assert_have_been_called lolcow
+	assert_have_been_called_with "foo bar" lolcow
+}
+
+function tear_down() {
+	# This will run after each test in this file
+	true
+}
+
+function tear_down_after_script() {
+	# This will run after all tests in this file
+	true
 }
